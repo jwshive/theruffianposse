1c51b10f6eb9aee949cffd0581cc2ba193e78dcf
Jason Shive <jwshive@gmail.com>
2020-07-09 15:58:56 -0400

    Trying to django migrate.

--- posse/admin.py
+++ posse/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import WarcraftLogsSettings, RaiderIOSettings, GuildNews, HelpfulGuildLinks, GuildAddonLinks, GuildApplications, BlizzardApiSettings, GuildInformation, GuildLeadership, GuildMOTD
+from .models import WarcraftLogsSettings, RaiderIOSettings, GuildNews, HelpfulGuildLinks, GuildAddonLinks, GuildApplications, BlizzardApiSettings, GuildInformation, GuildLeadership, GuildMOTD, ShadowlandsClassChart
 
 admin.site.register(WarcraftLogsSettings)
 admin.site.register(RaiderIOSettings)
@@ -11,3 +11,4 @@ admin.site.register(BlizzardApiSettings)
 admin.site.register(GuildInformation)
 admin.site.register(GuildLeadership)
 admin.site.register(GuildMOTD)
+admin.site.register(ShadowlandsClassChart)
